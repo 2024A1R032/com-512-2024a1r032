3afd10ac529e3c2ac980161e618b76285156dbdd
2024A1R032 <2024A1R032@mietjammu.in>
2026-08-25 12:34:26 +0530

    Create program14.py

---
+++ program14.py
@@ -0,0 +1,9 @@
+a=int(input("enter the value of first number: "))
+b=int(input("enter the value of second number:" ))
+temp=a
+a=b
+b=temp
+print(f"the value of a is : {a} ")
+print(f"the value of b is : {b} ")
+
+

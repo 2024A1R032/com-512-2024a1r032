c978c86688e541b6b5f0f579ec30669f6cf34f0f
2024A1R032 <2024A1R032@mietjammu.in>
2026-09-08 12:33:17 +0530

    Update program48.py

--- program48.py
+++ program48.py
@@ -1,5 +1,17 @@
-pin=input("enter the pin: ")
-if len(pin)==4 and pin.isdigit():
-    print("the lock is opened")
-else:
-    print("wrong pin ! enter again ")
+correct_pin="2850"
+while True:
+    pin=input("enter 4 digit pin: ")
+    if len(pin)!=4:
+        print("pin must be exactly 4 digits")
+        continue
+    if pin==correct_pin:
+            print("lock opened")
+            break
+    else:
+        print("wrong pin.try again")    
+# pin=input("enter the 4- digit pin: ")
+# if len(pin)==4 and pin.isdigit():
+#     print("the lock is opened")
+# else:
+#     print("wrong pin ! enter again ")
+

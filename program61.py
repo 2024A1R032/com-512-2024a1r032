335b9818adf85267b2f1b9384ad8bf952d0b879c
2024A1R032 <2024A1R032@mietjammu.in>
2026-09-15 12:06:42 +0530

    Update program61.py

--- program61.py
+++ program61.py
@@ -1,11 +1,13 @@
-# python program to repeatedly calculate the sum of digits of a number until the result becomes a single digit
-num = int(input("Enter a number: "))
-while num>=10:
-    sum_digits=0
-    while num>0:
-        digit=num%10
-        sum_digits=sum_digits+digit
-        num=num//10
-        num=sum_digits
-print("Single digit result: ",num)
+'''
+wap to repeatedly calculate the sum of digits of a nu. until the result becomes a single digit.
+'''
+n = int(input("Enter a number: "))
 
+while n > 9:
+    total = 0
+    while n > 0:
+        total += n % 10
+        n //= 10
+    n = total
+
+print("Single digit result:", n)
